c025a4ce7320fd42221af8b101f13f085c319090
OlehParyshkura2 <o.paryshkura@inc4.net>
2024-04-02 12:31:24 +0300

    Revert "fix lint"
    This reverts commit 298d0fbd3909c7b2aae0e033f58da66459b14c55.

--- services/migration/migration.go
+++ services/migration/migration.go
@@ -53,14 +53,11 @@ func historicalNotificationMigration(db *mongo.Client, dbName string, logger *za
 
 	indexModel := mongo.IndexModel{
 		Keys: bson.D{
-			{Key: "watcher_id", Value: 1},
-			{Key: "notification.timestamp", Value: -1},
+			{"watcher_id", 1},
+			{"notification.timestamp", -1},
 		},
 	}
 	_, err := historyCollection.Indexes().CreateOne(context.Background(), indexModel)
-	if err != nil {
-		return err
-	}
 
 	cursor, err := watcherCollection.Find(context.Background(), bson.M{})
 	if err != nil {
