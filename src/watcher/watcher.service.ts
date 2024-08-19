f9b3dcaf0dc12d1dcf137382d474df1182e6a310
serezhaolshan <olshan228@gmail.com>
2024-08-19 18:15:13 +0300

    fix: handleCallback method

--- src/watcher/watcher.service.ts
+++ src/watcher/watcher.service.ts
@@ -255,12 +255,10 @@ export class WatcherService {
     // @dev Did it for hiding ERC-1155 and ERC-721 transfers for users
     if (token && !(await isERC20Standard(token.address))) return;
 
-    console.log("after isERC20Standard check");
-
     const cutAddress = (addr: string) => addr ? `${addr.slice(0, 5)}...${addr.slice(-5)}` : "";
 
     const roundedAmount = Number(value.ether).toFixed(2);
-    const tokenSymbol = value.symbol ? value.symbol : "";
+    const tokenSymbol = value.symbol ?? (token ? token.symbol : "AMB");
 
     const data = {
       type: "transaction-alert",
