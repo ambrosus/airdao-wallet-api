2f536d9ef420956e93219c99e03ad4b78b05b5d9
serezhaolshan <olshan228@gmail.com>
2024-08-28 13:06:10 +0300

    fix: price decrease notifications

--- src/price-watchers/price-watcher.ts
+++ src/price-watchers/price-watcher.ts
@@ -45,7 +45,7 @@ export class PriceWatcher {
 
       const percentage: number = (currentPrice - watcher.tokenPrice) / watcher.tokenPrice * 100;
 
-      const roundedPercentage: number = Math.abs((Math.round(percentage * 100) / 100));
+      const roundedPercentage: number = Math.round(percentage * 100) / 100;
       if (roundedPercentage < watcher.threshold) return;
 
       const roundedPrice: string = currentPrice.toFixed(5);
