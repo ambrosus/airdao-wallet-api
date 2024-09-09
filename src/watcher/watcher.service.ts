164778343273012dc49dcb42b3752698d0fc49c4
serezhaolshan <olshan228@gmail.com>
2024-09-09 12:21:02 +0300

    feat: add logs for debug

--- src/watcher/watcher.service.ts
+++ src/watcher/watcher.service.ts
@@ -253,6 +253,8 @@ export class WatcherService {
     // @dev To avoid sending notifications for node rewards transactions
     // @dev To lower case for case-insensitive comparison
     if (from.toLowerCase() === rewardsBankAddress.toLowerCase()) return;
+    console.log("Addresses Comparsion: ", from.toLowerCase(), rewardsBankAddress.toLowerCase());
+
     console.log("After check Tx Data: ", from, to, type);
 
     // @dev Did it for hiding ERC-1155 and ERC-721 transfers for users
