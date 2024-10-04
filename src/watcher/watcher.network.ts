20a2a9fd4a7bff03ab6cca52a414222d8a0e3ebc
serezhaolshan <olshan228@gmail.com>
2024-10-04 13:54:41 +0300

    fix: getWatcherHistoricalPrices method response |4

--- src/watcher/watcher.network.ts
+++ src/watcher/watcher.network.ts
@@ -24,7 +24,7 @@ export class WatcherNetwork {
   async getWatcherHistoricalPrices(req: Request, res: Response): Promise<void> {
     try {
       const prices = await this.service.getWatcherHistoryPrices();
-      res.json({ prices: [JSON.parse(prices)] });
+      res.json({ prices: JSON.parse(prices) });
     } catch (error) {
       this.handleErrorResponse(res, error);
     }
